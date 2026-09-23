5b09e253cdba7454c0891cfc2fcd7f941daa28b6
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2026-09-23 14:42:51 -0400

    Initial setup complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './style.css'
+
+export async function askOpenAI() 
+{
+  let prompt=document.getElementById("query")!.innerHTML;
+
+  const result = await fetch("/api/openai", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt,
+    }),
+  });
+
+  const data = await result.json();
+
+  alert(data.response);
+  document.getElementById("response")!.innerHTML=data.response;
+}
