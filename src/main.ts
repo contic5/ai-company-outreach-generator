80faae42e1bed06ef1157b587dfc7323a345c78f
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2026-09-25 11:59:29 -0400

    Set up completed queries

--- src/main.ts
+++ src/main.ts
@@ -49,16 +49,43 @@ export function handle_submit()
     return;
   }
 
+  let knowledge="";
+  const knowledge_checkboxes=document.getElementsByName("knowledge_checkbox");
+  for(let i=0;i<knowledge_checkboxes.length;i++)
+  {
+    let knowledge_checkbox=knowledge_checkboxes[i] as HTMLInputElement;
+    let knowledge_dict_key=knowledge_checkbox.value;
+    if(knowledge_checkbox.checked)
+    {
+      knowledge+=knowledge_dict[knowledge_dict_key]+"\n";
+    }
+  }
+
   let prompt=`
-  Write a company outreach letter from Spectrum Works to ${company}. The goal is for ${company} to ${goal}.
-  `;  
+  Write a company outreach letter from Spectrum Works to ${company}. The goal is for ${company} to ${goal}. Context about Spectrum Works: ${knowledge}
+  `;
+
+  let prompt_short=prompt;
+  for(let knowledge_dict_key of knowledge_dict_keys)
+  {
+    prompt_short=prompt_short.replace(knowledge_dict[knowledge_dict_key],`[${knowledge_dict_key}]`);
+  }
   
   const query_element=document.getElementById("query") as HTMLDivElement;
-  query_element.innerHTML=prompt;
+  query_element.innerHTML=prompt_short;
   if(running_prompt)
   {
     askOpenAI(prompt);
   }
+}
+function toTitleCase(text:string)
+{
+  let words=text.split(" ");
+  for(let i=0;i<words.length;i++)
+  {
+    words[i]=words[i].slice(0,1).toUpperCase()+words[i].slice(1,words[i].length);
+  }
+  return words.join(" ");
 }
 export function update_goal()
 {
@@ -70,8 +97,31 @@ export function update_goal()
     goal_element.innerHTML=sample_goal;
   }
 }
+export function load_checkboxes()
+{
+  let knowledge_checkbox_div=document.getElementById("knowledge_checkbox_div") as HTMLDivElement;
+  for(let knowledge_dict_key of knowledge_dict_keys)
+  {
+    let div=document.createElement("div");
+    knowledge_checkbox_div.appendChild(div);
 
-const running_prompt=false;
+    let label_text=knowledge_dict_key;
+    label_text=label_text.replace("_"," ");
+    label_text=toTitleCase(label_text);
+
+    let label=document.createElement("label");
+    div.appendChild(label);
+    label.innerHTML=label_text;
+
+    let checkbox=document.createElement("input");
+    div.appendChild(checkbox);
+    checkbox.type="checkbox";
+    checkbox.name="knowledge_checkbox";
+    checkbox.value=knowledge_dict_key;
+  }
+}
+
+const running_prompt=true;
 let knowledge_dict:Record<string,string>={};
 let knowledge_dict_keys=["company_benefits","employment_model","goals","impact","partner_companies","partner_quotes","values"];
 get_context();
